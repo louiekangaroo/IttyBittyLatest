c102c3ee04f9bb192cc38823d5c115019313ea6a
esowelow <esowelow@gmail.com>
2015-03-08 00:15:10 +0800

    Reformatted

--- Itty Bitty Childrens Expo/process_registration.php
+++ Itty Bitty Childrens Expo/process_registration.php
@@ -1,6 +1,7 @@
 <?php 
 require_once "Mail.php";
 require_once "Mail/mime.php";
+require_once 'lib/swift_required.php';
 require("connection.php");
 global $con;
 
@@ -24,18 +25,33 @@ $clean_eadd = stripslashes($eadd);
 $clean_remarks = stripslashes($remarks);
 $clean_regtype = stripslashes($regtype);
 
+$sql = ("CALL InsertRegistration('$clean_compname','$clean_comptype','$clean_fname','$clean_lname','$clean_jobtitle','$clean_pnumber','$clean_eadd','$clean_remarks','$clean_regtype')");
 
-	 $sql = ("CALL InsertRegistration('$clean_compname','$clean_comptype','$clean_fname','$clean_lname','$clean_jobtitle','$clean_pnumber','$clean_eadd','$clean_remarks','$clean_regtype')");
-          $addregister = $con->query($sql);
+$addregister = $con->query($sql);
 
-	 if($addregister == false){
+	 if($addregister == false)
+     {
 	   echo"An error has occured".mysql_error();
-	 }else{
-
-$From = "Itty Bitty <esowelow@gmail.com>";
-$to = "Administrator <louiedbest@gmail.com>";
-$subject = "Registration for interest\r\n\r\n";
-$body = "<b>Company/Organisation Name: </b>".$clean_compname."<br><br>"
+	 }
+     else
+     {
+         
+$transport = Swift_SmtpTransport::newInstance('smtp.mandrillapp.com', 587)
+        ->setUsername('louiedbest@gmail.com')
+        ->setPassword('pXC6c47zRA17xmQneipLOQ');
+         
+// Create the message
+$message = Swift_Message::newInstance();
+//receiver
+$message->setTo('esowelow@gmail.com');
+//sender
+$message->setFrom('louiedbest@gmail.com');
+//subject
+$message->setSubject('Itty Bitty Registration');
+//msg lenght
+$message->setMaxLineLength(10000);
+         
+$message->setBody("<b>Company/Organisation Name: </b>".$clean_compname."<br><br>"
 ."<b>Company/Organisation Type: </b>".$clean_comptype."<br><br>"
 ."<b>First Name: </b>".$clean_fname."<br><br>"	
 ."<b>Last Name: </b>".$clean_lname."<br><br>"  
@@ -43,37 +59,18 @@ $body = "<b>Company/Organisation Name: </b>".$clean_compname."<br><br>"
 ."<b>Phone Number: </b>".$clean_pnumber."<br><br>"
 ."<b>Contact Email: </b>".$clean_eadd."<br><br>"
 ."<b>Remarks: </b>".$clean_remarks."<br><br>"
-."<b>Interested in : </b>".$clean_regtype."<br><br><br><br>"
-."<i><b>Note:</b> This e-mail is automatically generated. Please do not reply.</i>";
-$host = "smtp.mandrillapp.com";
-$port = "587";					 // should be 25 by default, but needs to be whichever port the mail server will be using for smtp 
-$timeout = "45";				 // typical timeout. try 45 for slow servers
-$username = "louiedbest@gmail.com"; // the login for your smtp
-$password = "pXC6c47zRA17xmQneipLOQ";			// the password for your smtp
- 
-$headers = array ('From' => $From,
-  'To' => $to,
-  'Subject' => $subject);
-
- $mime = new Mail_mime();
-        $mime->setHTMLBody($body);
-
-        $body = $mime->get();
-        $headers = $mime->headers($headers);
-//$headers= implode("\r\n", $headers);
-$smtp = Mail::factory('smtp',
-  array ('host' => $host,
-    'port' => $port,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password));
- 
-$mail = $smtp->send($to, $headers, $body);
- 
-if (PEAR::isError($mail)) {
-  echo("<p>" . $mail->getMessage() . "</p>");
-} 
-		//echo "record successfully added!";	
-	 }
-
+."<b>Interested in : </b>".$clean_regtype."<br><br><br>"
+."<i><b>Note:</b> This e-mail is automatically generated. Please do not reply.</i>",'text/html');
+                
+$mailer = Swift_Mailer::newInstance($transport);
+$send = $mailer->send($message);
+   }
+        if(!$send)
+        {
+            echo "msg not send";
+        }
+        else
+        {
+            echo "msg send";
+        } 
 ?>
